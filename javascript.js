0b70a2fcc1b0222d9e8c6659f75813ac7b741c3d
kirigaya7447 <joaoh2915@gmail.com>
2024-04-08 21:38:43 -0300

    Criado a base do site

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("A");
+function agendar(){
+    let titulo = document.getElementById("tituloTarefa").value;
+    let descricao = document.getElementById("descricaoTarefa").value;
+    const tarefas = document.getElementById("listaTarefas");
+
+    let li = document.createElement("li");
+    li.innerHTML = titulo;
+
+    tarefas.append(li);
+
+    let ol = document.createElement("ol");
+    ol.type = "a";
+
+    tarefas.append(ol);
+
+    let li2 = document.createElement("li");
+    li2.innerHTML = descricao;
+    ol.append(li2);
+
+    document.getElementById("tituloTarefa").value = null;
+    document.getElementById("descricaoTarefa").value = null;
+}
